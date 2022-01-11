23330c44b90fcd8c829850dc4049e36b85e23fef
csalvesen84 <73505297+csalvesen84@users.noreply.github.com>
2022-01-10 17:10:38 -0700

    Add project files.

---
+++ MyFirstASPNETWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,7 @@
+﻿$("#btnSend").click(function () {
+    alert("From: " + $("#textFrom").val() + "\nSubject: " + $("#textSubject").val() + "\n\n" + $("#textMessage").val());
+    for (let i = 0; i < 10; i++) {
+        $("#picAnderson").fadeOut("slow");
+        $("#picAnderson").fadeIn("slow");
+    }
+})
