dfa6c64a7d98e84a11a770697e98b9f8bfcc56f8
csalvesen84 <73505297+csalvesen84@users.noreply.github.com>
2022-01-10 17:11:33 -0700

    made it flash more

--- MyFirstASPNETWebApp/wwwroot/js/FanMail.js
+++ MyFirstASPNETWebApp/wwwroot/js/FanMail.js
@@ -1,6 +1,6 @@
 ﻿$("#btnSend").click(function () {
     alert("From: " + $("#textFrom").val() + "\nSubject: " + $("#textSubject").val() + "\n\n" + $("#textMessage").val());
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 12; i++) {
         $("#picAnderson").fadeOut("slow");
         $("#picAnderson").fadeIn("slow");
     }
